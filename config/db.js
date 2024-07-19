644ca0b5a219d63c55a2387e30a4a3f2ba176381
OpenClassrooms <noam@leprovost.pro>
2024-07-19 14:57:16 +0200

    Ajout de la connexion à la base de donnée, création des dossiers 'routes' et 'config'

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const dbHost = "ocp7.mottm2d.mongodb.net";
+const dbAppName = "OCP7";
+const dbUsername = "noamocp7user";
+const dbPassword = "d4ReplTDH0loEhz9";
+
+const dbUri = `mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/?retryWrites=true&w=majority&appName=${dbAppName}`;
+
+const client = new MongoClient(dbUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected successfully to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = { connectToDatabase, client };
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const { connectToDatabase } = require('./config/db');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 
-app.post('/api/auth/signup', (req, res) => {
-  res.send('OK');
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+  connectToDatabase(); 
 });
