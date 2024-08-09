416c3936da6a21d71d84b0dcedbdf33ee267c8ed
OpenClassrooms <noam@leprovost.pro>
2024-08-09 16:54:26 +0200

    Ajout de la route 'login' avec système de signature de token.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors'); 
 
+require("dotenv").config({ path: "./config/.env" });
+
 const { connectToDatabase } = require('./config/db');
 const authRoutes = require('./routes/auth');
 
